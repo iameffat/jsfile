342c6a6bba501561fdf1f551c0a84b863db08ceb
iameffat <53122076+iameffat@users.noreply.github.com>
2022-08-14 03:13:14 +0600

    Update adtest.js

--- adtest.js
+++ adtest.js
@@ -1,1 +1,1 @@
-document.write('<a href="https://cutt.ly/deenelife"><img src="https://www.bdniyog.com/wp-content/uploads/2022/08/deenelife-ad.jpg"/></a>');
+document.write('<a href="https://cutt.ly/deenelife" target="_blank"><img src="https://www.bdniyog.com/wp-content/uploads/2022/08/deenelife-ad.jpg" width="350" height="310"/></a>');
